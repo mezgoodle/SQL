e1aff4f1ba3b365422f3fced2eba14f76109880f
mezgoodle <mezgoodle@gmail.com>
2019-06-10 19:20:08 +0300

    Update index.sql

--- index.sql
+++ index.sql
@@ -14,3 +14,10 @@ ALTER TABLE people DROP COLUMN pass VARCHAR(32);
 INSERT INTO people (name, bio, birth) VALUES('Maxim', 'Programmer', '2001-09-11');
 INSERT INTO people (name, birth) VALUES('Max', '2001-01-11');
 ALTER TABLE people CHANGE birth birth DATE NOT NULL;
+INSERT INTO people (name, bio, birth)
+		VALUES
+		('Bob', 'ffff', '2020'),
+		('Bob1', 'ffff', '2020'),
+		('Bob2', 'ffff', '2020'),
+		('Bob34', 'ffff', '2020'),
+		('Bob3', 'ffff', '2020');
