452a159cb39c14c692dcdd541b8af2da5ac7df0e
mezgoodle <mezgoodle@gmail.com>
2019-06-25 08:27:30 +0300

    Update index.sql

--- index.sql
+++ index.sql
@@ -93,4 +93,5 @@ ORDER BY people.name DESC;
 SELECT orders.date_time, people.name FROM orders
 RIGHT JOIN people ON orders.id = people.id;
 
-SELECT name AS `Імя`, birth AS `День народження` FROM people
+SELECT name AS `Імя`, birth AS `День народження` FROM people;
+SELECT CONCAT('Імя: ', name, '. День народження', birth)  FROM people;
