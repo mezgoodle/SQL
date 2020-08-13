8f58b935b5abddef52144a1612672da86bd31b96
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-13 16:26:50 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 # seed the pseudorandom number generator
 from random import seedfrom random import random
-# seed random number generator
+
 seed(1)
-# generate some random numbers
+
 print(random(), random(), random())
-# reset the seed
+
 seed(1)
-# generate some random numbers
+
+
+
+
+
+
+
+
+
+
+
+
+
 print(random(), random(), random())
