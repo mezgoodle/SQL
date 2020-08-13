446797d85b38b29d78e2a7447b36cfe51b2805c8
mezgoodle <mezgoodle@gmail.com>
2020-08-13 13:34:35 +0000

    test autopep8

--- test.py
+++ test.py
@@ -1,52 +1,15 @@
 # seed the pseudorandom number generator
 
 
-
-
-
-
-
-
-
-
-
 from random import seedfrom random import random
 
 seed(1)
 
 
-
-
-
-
-
-
-
-
-
-
-
 print(random(), random(), random())
 
 
-
-
-
-
-
-
-
-
-
 seed(1)
-
-
-
-
-
-
-
-
 
 
 print(random(), random(), random())
