04648f6823be97ce23f683d88ebf64c0a4e86b66
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-03 21:52:15 +0300

    Update index.sql

--- index.sql
+++ index.sql
@@ -11,7 +11,7 @@ CREATE TABLE people(
 ALTER TABLE people ADD pass VARCHAR(32);
 ALTER TABLE people DROP COLUMN pass VARCHAR(32);
 
-INSERT INTO people (name, bio, birth) VALUES('Maxim', 'Programmer', '2001-09-11');
+INSERT INTO people (name, bio, birth) VALUES('Maxim', 'Programer', '2001-09-11');
 INSERT INTO people (name, birth) VALUES('Max', '2001-01-11');
 ALTER TABLE people CHANGE birth birth DATE NOT NULL;
 INSERT INTO people (name, bio, birth)
